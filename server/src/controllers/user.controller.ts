ee26a0e27c817e3b271f2490cbfb36223c78aef5
dhairyash gupta <dhairyashgupta12@gmail.com>
2024-11-06 09:04:20 +0530

    feat(auth): add access/refresh token handling, refresh token controller, and routes
    - Implemented access and refresh token issuance during login.
    - Added refresh token controller to handle token renewal.
    - Created new routes for refresh token functionality.
    - Stored access and refresh tokens securely in HTTP-only cookies.
    - add auth middleware

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,8 +1,29 @@
 import prisma from "../prismaClient/prismaClient";
+import { CustomRequest } from "../types/customRequest";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 import { comparePassword, hashPassword } from "../utils/hashUtils";
+import jwt, { Secret } from "jsonwebtoken";
+
+const generateAccessAndRefreshToken = async (userId: number) => {
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+  const accessToken = jwt.sign(
+    {
+      id: user?.id,
+      email: user?.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET as Secret,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+
+  const refreshToken = jwt.sign(
+    { id: user?.id },
+    process.env.REFRESH_TOKEN_SECRET as Secret,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+  return { accessToken, refreshToken };
+};
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
@@ -67,11 +88,96 @@ export const loginUser = asyncHandler(async (req, res) => {
     throw new ApiError(401, "Password is incorrect");
   }
 
-  const { password: _, ...userWithoutPasword } = user;
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user.id
+  );
+  await prisma.user.update({ where: { id: user.id }, data: { refreshToken } });
+
+  const option = {
+    httponly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  const { password: _, refreshToken: __, ...userWithoutPasword } = user;
 
   return res
     .status(200)
+    .cookie("refreshToken", refreshToken, option)
+    .cookie("accessToken", accessToken, option)
     .json(
       new ApiResponse(200, userWithoutPasword, "User LoggedIn Successfully")
+    );
+});
+
+export const logOutUser = asyncHandler(async (req: CustomRequest, res) => {
+  const user = await prisma.user.update({
+    where: { id: req?.user?.id },
+    data: { refreshToken: null },
+  });
+
+  const option = {
+    httponly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  return res
+    .status(200)
+    .clearCookie("refreshToken", option)
+    .clearCookie("accessToken", option)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+export const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  const decodedToken = jwt.verify(
+    incomingRefreshToken,
+    process.env.REFRESH_TOKEN_SECRET as Secret
+  ) as { id: number };
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: decodedToken?.id,
+    },
+  });
+
+  if (!user) {
+    throw new ApiError(401, "Invaild refresh Token");
+  }
+
+  if (incomingRefreshToken !== user.refreshToken) {
+    throw new ApiError(401, "refreshToken is expired or used");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user.id
+  );
+  const option = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      refreshToken,
+    },
+  });
+
+  return res
+    .status(200)
+    .cookie("refreshToken", refreshToken, option)
+    .cookie("accessToken", accessToken, option)
+    .json(
+      new ApiResponse(
+        200,
+        { accessToken, refreshToken },
+        "AccessToken refreshed"
+      )
     );
 });
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller";
+import {
+  loginUser,
+  logOutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controller";
+import { verifyJWT } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
+
+// protected route
+router.route("/logout").post(verifyJWT, logOutUser);
 
 export default router;
