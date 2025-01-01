ed8aebd9bc89172a0200bd58a1c25596d998f6a7
dhairyash gupta <dhairyashgupta12@gmail.com>
2025-01-01 17:40:21 +0530

    update frontend origin

--- server/src/controllers/order.controller.ts
+++ server/src/controllers/order.controller.ts
@@ -128,8 +128,8 @@ export const createOrder = asyncHandler(async (req: CustomRequest, res) => {
     metadata: {
       orderId: unpaidOrder.id,
     },
-    success_url: `https://localhost:5173/orders?status=success`,
-    cancel_url: `https://localhost:5173/orders?status=failed`,
+    success_url: `${process.env.CORS_ORIGIN}/orders?status=success`,
+    cancel_url: `${process.env.CORS_ORIGIN}/orders?status=failed`,
     expires_at: Math.floor(Date.now() / 1000) + 1800,
   });
 
